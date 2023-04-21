cc05d715f9b6cf4a9c9c6ddae4bfea3135ef8f7b
Petar Tesic <petar.tesic@syrmia.com>
2023-04-21 10:57:44 +0200

    Final commit on guru remote repository!

---
+++ hello_from_guru.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void error(){
+  fprintf(sdterr,"-1\n");
+  exit(EXIT_FAILURE);
+}
+
+
+int main(int argc, char *argv[]){
+ 
+  printf("Hello git guru!\n");
+  printf("Hello almighty git guru!\n"); 
+
+  return 0;
+}
